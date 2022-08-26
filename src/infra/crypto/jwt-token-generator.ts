dc530f5f6ef857d97692f32e11bb7f1630db1254
diego nascimento <diegostealth3@gmail.com>
2022-08-26 16:02:32 -0300

    jwt token

---
+++ src/infra/crypto/jwt-token-generator.ts
@@ -0,0 +1,11 @@
+import { TokenGenerator } from "@/data/contracts/crypto"
+import { sign } from 'jsonwebtoken'
+
+export class JwtTokenGenerator {
+  constructor (private readonly secret: string) {}
+
+  async generateToken (params: TokenGenerator.params):Promise<TokenGenerator.Result> {
+    const expirationInSeconds = params.expirationInMs
+    return sign({ key: params.key, }, this.secret, { expiresIn: expirationInSeconds / 1000 })
+  }
+}
