a8dc073642ea6908a5bc6ac641fb5045203fca1a
diego nascimento <diegostealth3@gmail.com>
2022-08-26 15:07:02 -0300

    move axioshttpclient to its own file and axios tests

---
+++ src/infra/http/axios-client.ts
@@ -0,0 +1,11 @@
+import axios from "axios"
+import { HttpGetClient } from "./client"
+
+export class AxiosHttpClient implements HttpGetClient {
+  async get <T= any> (args: HttpGetClient.Params): Promise<T> {
+    const result = await axios.get(args.url, {
+      params: args.params
+    })
+    return result.data
+  }
+}
